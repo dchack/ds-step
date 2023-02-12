95e6dd97de06c8d65b5c632ffe591bac892043d7
dongchao <hdbysj@163.com>
2023-02-12 16:03:01 +0800

    java 8 book

--- src/main/java/com/hope/dp/java8/MethodReference.java
+++ src/main/java/com/hope/dp/java8/MethodReference.java
@@ -1,6 +1,7 @@
 package com.hope.dp.java8;
 
 import java.io.File;
+import java.util.function.Predicate;
 
 /**
  * 方法引用
@@ -17,6 +18,17 @@ public class MethodReference {
         File[] hiddenFiles = new File(".").listFiles(File::isHidden);
         for (File file : hiddenFiles) {
             System.out.printf(" " +file.getName());
+        }
+
+        // get lamdbda
+        predicateTest("", (s) -> {
+            return s.equals("1");
+        });
+    }
+
+    private static void predicateTest(String s, Predicate<String> predicate) {
+        if (predicate.test(s)) {
+            System.out.printf("" + "test ok");
         }
     }
 }
